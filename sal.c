3a2c0933b3eb6cb4060f0400a428d3f8c93e34a1
LucasDNiguti <166518261+LucasDNiguti@users.noreply.github.com>
2024-04-30 15:57:38 +0000

    30.04.2024 12:57

--- sal.c
+++ sal.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void nome(char *name){
+    printf("\nDigite seu nome:");
+    scanf("%s",name);
+}
+
+void salario(float *sal){
+    printf("\nDigite seu salário:");
+    scanf("%f",sal);
+}
+
+int main() {
+
+  char name[100];
+  float sal;
+
+ nome(name);
+ salario(&sal);
+
+ printf("\nNome: %s", name);
+  float Salperce = (sal * 28.5) / 100;
+  sal = sal + Salperce;
+ printf("\nSalario: %2.f",sal);
+
+ return 0;
+}
